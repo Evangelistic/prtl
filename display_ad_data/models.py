fe47f5c96acef791d071c0e5df37279505518305
vlva <geek.evangelista@gmail.com>
2017-07-28 09:20:07 +0300

    Compare user group function and small fixes

--- display_ad_data/models.py
+++ display_ad_data/models.py
@@ -8,8 +8,6 @@ import getpass, datetime
 
 def search_ad_data(inputSearch, typeSearch):
     server = Server(ad_server, get_info=ALL)
-    # user=input("Enter login: ")
-    # pwd=getpass.getpass("Enter pasword: ")
     user = ad_user
     pwd = ad_pwd
 
@@ -37,11 +35,7 @@ def search_ad_data(inputSearch, typeSearch):
     for i in range(3):
         del entry_list[0]
 
-    for entry in entry_list:
-        entry['dn'] = entry['dn'].split(',')[0][3:]
-
     ad_data=[]
-
     for entry in entry_list:
         temp = []
         if not entry['attributes']['lastLogon']:
@@ -51,6 +45,8 @@ def search_ad_data(inputSearch, typeSearch):
         if typeSearch == 'last_logon' and (
                         datetime.datetime.now().replace(tzinfo=None) - t.replace(tzinfo=None) < datetime.timedelta(days=int(inputSearch))):
             continue
+        temp.append([entry['dn']])
+        entry['dn'] = entry['dn'].split(',')[0][3:]
         temp.append([entry['dn']])
         temp.append([t.strftime('%m/%d/%Y')])
         temp.append([entry['attributes']['sAMAccountName']])
@@ -99,5 +95,37 @@ def search_ad_data_group(inputSearch, typeSearch):
             listmembers.append(member[3:].split(sep=',')[0])
         temp.append(listmembers)
         ad_data.append(temp)
+
+    return ad_data
+
+
+def compare_user_group(inputSearch):
+    server = Server(ad_server, get_info=ALL)
+    user = ad_user
+    pwd = ad_pwd
+    c = Connection(server, user=user, password=pwd, authentication=NTLM, auto_bind=True)
+    ad_data = {}
+    for adlogin in inputSearch:
+        cn = search_ad_data(adlogin, 'login')[0][0][0]
+        print(cn)
+        ad_filter = '(member=' + cn + ')'
+
+        entry_list = c.extend.standard.paged_search(search_base='DC=corp,DC=willesden,DC=by',
+                                                    search_filter='(&(objectCategory=CN=Group,CN=Schema,CN=Configuration,DC=corp,DC=willesden,DC=by)' + ad_filter + ')',
+                                                    search_scope=SUBTREE,
+                                                    attributes=['cn', 'description', 'member', 'whenChanged',
+                                                                'whenCreated'],
+                                                    paged_size=5,
+                                                    generator=False)
+
+        for i in range(3):
+            del entry_list[0]
+
+        for entry in entry_list:
+            entry['dn'] = entry['dn'].split(',')[0][3:]
+        temp = []
+        for entry in entry_list:
+            temp.append(entry['attributes']['cn'])
+        ad_data[adlogin] = temp
 
     return ad_data
--- display_ad_data/views.py
+++ display_ad_data/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import search_ad_data, search_ad_data_group
+from .models import search_ad_data, search_ad_data_group, compare_user_group
 from django.views.decorators.http import require_GET
 from django.contrib.auth.decorators import login_required
 
@@ -19,8 +19,32 @@ def ad_search(request):
         return render(request, 'portal/ad-search.html', {
             'title': 'AD Search', })
     else:
-        if typeSearch == 'group':
-            #categorySearch = r'Group'
+        if typeSearch == 'compare':
+            inputSearch = inputSearch.split()
+            print(inputSearch)
+            data = compare_user_group(inputSearch)
+            print(len(data['vlva']))
+            print(len(data['das']))
+            magicset = set([])
+            for i in data:
+                print(data[i][0])
+                magicset.update(data[i])
+            magiclist = []
+            for i in magicset:
+                temp = [[i]]
+                for j in data:
+                    if i in data[j]:
+                        temp.append('+')
+                    else:
+                        temp.append('-')
+                magiclist.append(temp)
+            content = {'title': 'Compare AD User',
+                       'rowName': 'Compare User Group',
+                       'colName': ['Group name'] + inputSearch,
+                       'data': magiclist,
+                       'len': len(data)}
+            render(request, 'portal/ad-search.html', content)
+        elif typeSearch == 'group':
             data = search_ad_data_group(inputSearch, typeSearch)
             content = {'title': 'AD Search',
                        'rowName': 'Group credentials',
@@ -28,9 +52,9 @@ def ad_search(request):
                        'data': data,
                        'len': len(data)}
         else:
-            #categorySearch = r'Person'
             data = search_ad_data(inputSearch, typeSearch)
-
+            '''delete  dn user attribute  in output'''
+            data = [data[i][1:] for i in range(len(data))]
             content = {'title': 'AD Search',
                        'rowName': 'User credentials',
                        'colName': ['Full name', 'Last logon', 'Login', 'Email'],
