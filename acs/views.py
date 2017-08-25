dddefb3a4f9722ed8dc885e6be57709fb78f42bb
Andrei <3636923@gmail.com>
2017-08-25 11:35:41 +0300

    fix render url

--- acs/views.py
+++ acs/views.py
@@ -3,6 +3,6 @@ from django.conf import settings
 # Create your views here.
 
 
-def acs():
+def acs(request):
     content = {'url': settings.ACS_URL}
-    return render_to_response('portal/contact-us.html', content)
+    return render_to_response('portal/acs.html', content)
