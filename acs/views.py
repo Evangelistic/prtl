a1f72451a34b6edcd2d24bcff7a92f7be95e6903
Andrei <3636923@gmail.com>
2017-08-25 11:28:19 +0300

    create views

--- acs/views.py
+++ acs/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render_to_response
+from django.conf import settings
 # Create your views here.
+
+
+def acs():
+    content = {'url': settings.ACS_URL}
+    return render_to_response('portal/contact-us.html', content)
